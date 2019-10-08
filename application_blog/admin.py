2bb564e9c58a76733f558bebacea55cfbbe1dd0b
Markus Euchner <markus.euchner@sovanta.com>
2019-10-08 17:07:09 +0200

    feat(application):add model to create and publish posts

--- application_blog/admin.py
+++ application_blog/admin.py
@@ -1,15 +1,4 @@
 from django.contrib import admin
-from .models import Choice, Question
+from .models import Post
 
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
-
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-            (None,               {'fields': ['question_text']}),
-            ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-                ]
-    inlines = [ChoiceInline]
-
-admin.site.register(Question, QuestionAdmin)
+admin.site.register(Post)
--- application_blog/views.py
+++ application_blog/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
-from .models import Question, Choice
+#from .models import Question, Choice
 
 
 def index(request):
